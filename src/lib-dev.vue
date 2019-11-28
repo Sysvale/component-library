38952e282424f8f74013bd27ad8a8ad1fb91568e
Rafael Dias <rdias.ga@gmail.com>
2019-11-28 09:21:06 -0300

    add library structure sample code

---
+++ src/lib-dev.vue
@@ -0,0 +1,23 @@
+<script>
+import { ComponentLibrarySample } from '@/entry';
+export default {
+    name:  'ComponentLibraryDev', // vue library dev component
+    components: {
+        ComponentLibrarySample,
+    },
+}
+</script>
+
+<template>
+    <div class="component-library-dev">
+        <component-library-sample></component-library-sample>
+    </div>
+</template>
+
+<style scoped>
+    .component-library-dev {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+    }
+</style>
