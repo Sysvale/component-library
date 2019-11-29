34b14f55e9cb341642bcbf320a2df930d390afbf
Rafael Dias <rdias.ga@gmail.com>
2019-11-29 05:40:07 -0300

    solves eslint problems in entry.js file

--- src/entry.js
+++ src/entry.js
@@ -1,30 +1,29 @@
-// Import vue components
-import * as components from '@/components/index';
+import * as components from './components/index';
 
 // install function executed by Vue.use()
 function install(Vue) {
-  if (install.installed) return;
-  install.installed = true;
-  Object.keys(components).forEach((componentName) => {
-    Vue.component(componentName, components[componentName]);
-  });
+	if (install.installed) return;
+	install.installed = true;
+	Object.keys(components).forEach((componentName) => {
+		Vue.component(componentName, components[componentName]);
+	});
 }
 
 // Create module definition for Vue.use()
 const plugin = {
-  install,
+	install,
 };
 
 // To auto-install when vue is found
 /* global window global */
 let GlobalVue = null;
 if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
+	GlobalVue = window.Vue;
 } else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
+	GlobalVue = global.Vue;
 }
 if (GlobalVue) {
-  GlobalVue.use(plugin);
+	GlobalVue.use(plugin);
 }
 
 // Default export is library as a whole, registered via Vue.use()
@@ -32,4 +31,4 @@ export default plugin;
 
 // To allow individual component use, export components
 // each can be registered via Vue.component()
-export * from '@/components/index';
+export * from './components/index';
