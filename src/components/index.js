5a106e9a673e2be00bf81eb6c33973c1267f8b23
Rafael Dias <rdias.ga@gmail.com>
2019-12-10 15:18:08 -0300

    Exporta o componente do steppercounter

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Badge } from './Badge.vue';
 export { default as CustomWrapper } from './Wrapper.vue';
+export { default as StepperCounter } from './StepperCounter.vue';
