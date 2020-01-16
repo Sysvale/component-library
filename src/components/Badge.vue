5af557ca522d8c74170d7e1b6da4946562802837
Rafael Dias <rdias.ga@gmail.com>
2020-01-16 08:30:58 -0300

    Adds 'bgColor', variantMode, ColorCodeMode and variant props in the badge component

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -33,6 +33,36 @@ export default {
 				you can pass the hex value of the text
 				color of the badge.`,
 	},
+		bgColor: {
+			type: String,
+			default: '#28a74526',
+			description: `If the color code mode is set,
+				you can pass the hex value of the background
+				color of the badge.`,
+		},
+		variantMode: {
+			type: Boolean,
+			default: true,
+			description: `The property that specifies 
+				if the badge will work in variant mode
+				or not.`,
+		},
+		colorCodeMode: {
+			type: Boolean,
+			default: false,
+			description: `The property that specifies 
+				if the badge will work in the color code
+				mode or not.`,
+		},
+		variant: {
+			type: String,
+			default: 'variant-1',
+			description: `Variant mode gives 6 predefined badges 
+				to work with. Seting this property to 'true',
+				you can use 'variant-1', 'variant-2', ... 'variant-6'
+				to use the predefined badge styles.`,
+		},
+	},
 
 	computed: {
 		style() {
