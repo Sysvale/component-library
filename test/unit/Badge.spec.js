345a3d2aedf2b2b65cd947750d68185afd760883
Rafael Dias <rdias.ga@gmail.com>
2020-01-16 08:41:43 -0300

    Updates the badge tests

--- test/unit/Badge.spec.js
+++ test/unit/Badge.spec.js
@@ -15,42 +15,91 @@ test('Component is mounted properly', () => {
 	expect(wrapper).toMatchSnapshot();
 });
 
-describe("Computed property 'style' test", () => {
-	test('if the computed property style works properly with the default value for the prop status_type', () => {
+describe("Computed property 'predefinedStyle' test", () => {
+	test('if the computed property predefinedStyle works properly with the default value for the prop variant', () => {
 		const wrapper = mount(Badge, {
 			localVue,
 			propsData: {
 				content: 'Test',
 			},
 		});
-		expect(wrapper.vm.style).toBe('negative-badge');
+		expect(wrapper.vm.predefinedStyle).toBe('variant-1');
 	});
 });
 
-describe("Prop 'status_type' tests", () => {
-	test("if the computed property changes when the prop status_type is setted to 'negative'", () => {
+describe("Prop 'variant' tests", () => {
+	test("if the computed property changes when the prop variant is setted to 'variant-1'", () => {
 		const wrapper = mount(Badge, {
 			localVue,
 			propsData: {
-				status_type: 'Negative',
+				variant: 'variant-1',
 				content: 'Test',
 			},
 		});
 
-		expect(wrapper.vm.style).toBe('negative-badge');
+		expect(wrapper.vm.predefinedStyle).toBe('variant-1');
 	});
 
-	test("if the computed property changes when the prop status_type is setted to 'positive'", () => {
+	test("if the computed property changes when the prop variant is setted to 'variant-2'", () => {
 		const wrapper = mount(Badge, {
 			localVue,
 			propsData: {
-				status_type: 'Positive',
+				variant: 'variant-2',
 				content: 'Test',
 			},
 		});
 
-		expect(wrapper.vm.style).toBe('positive-badge');
+		expect(wrapper.vm.predefinedStyle).toBe('variant-2');
 	});
+
+	test("if the computed property changes when the prop variant is setted to 'variant-3'", () => {
+		const wrapper = mount(Badge, {
+			localVue,
+			propsData: {
+				variant: 'variant-3',
+				content: 'Test',
+			},
+		});
+
+		expect(wrapper.vm.predefinedStyle).toBe('variant-3');
+	});
+
+	test("if the computed property changes when the prop variant is setted to 'variant-4'", () => {
+		const wrapper = mount(Badge, {
+			localVue,
+			propsData: {
+				variant: 'variant-4',
+				content: 'Test',
+			},
+		});
+
+		expect(wrapper.vm.predefinedStyle).toBe('variant-4');
+	});
+
+	test("if the computed property changes when the prop variant is setted to 'variant-5'", () => {
+		const wrapper = mount(Badge, {
+			localVue,
+			propsData: {
+				variant: 'variant-5',
+				content: 'Test',
+			},
+		});
+
+		expect(wrapper.vm.predefinedStyle).toBe('variant-5');
+	});
+
+	test("if the computed property changes when the prop variant is setted to 'variant-6'", () => {
+		const wrapper = mount(Badge, {
+			localVue,
+			propsData: {
+				variant: 'variant-6',
+				content: 'Test',
+			},
+		});
+
+		expect(wrapper.vm.predefinedStyle).toBe('variant-6');
+	});
+});
 
 	test("if the computed property changes when the prop status_type is setted to 'neutral'", () => {
 		const wrapper = mount(Badge, {
