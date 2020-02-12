fc30c29c138f424dac1f90f076aedc51163efe61
Lucas Nascimento <lucas.barbosa.nas@gitlab.com>
2020-02-12 15:13:47 -0300

    Cria arquivo vue para paginação

---
+++ src/components/Pagination.vue
@@ -0,0 +1,22 @@
+<template>
+	<div
+		class=""
+	>
+		<small class="regular">
+			{{ content }}
+		</small>
+	</div>
+</template>
+
+<script>
+
+export default {
+	props: {
+		
+	},
+
+};
+</script>
+<style>
+
+</style>
