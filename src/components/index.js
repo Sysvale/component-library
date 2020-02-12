ec9526e4a919700497ebc4cfa27c3bcd009b65bd
Rafael Dias <rdias.ga@gmail.com>
2020-02-12 13:31:17 -0300

    Exports the table component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 export { default as Badge } from './Badge.vue';
 export { default as StepperCounter } from './StepperCounter.vue';
 export { default as ActionBar } from './ActionBar.vue';
+export { default as Table } from './Table.vue';
