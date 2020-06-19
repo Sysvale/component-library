565ba7221b53b8ab2d7df2ee2426c40d6dd42fa7
Tássio Caique <tassiok2@gmail.com>
2020-06-19 17:09:07 -0300

    Add - Estilização e comportamento básico do componente Nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,16 @@
 <template>
-	 <b-nav>
-		<b-nav-item active>Active</b-nav-item>
-		<b-nav-item>Link</b-nav-item>
-		<b-nav-item>Another Link</b-nav-item>
-		<b-nav-item disabled>Disabled</b-nav-item>
+	 <b-nav
+	 	class="px-2"
+	 >
+		<b-nav-item
+			v-for="item in computedItems"
+			:key="item.label"
+			:active="activeItem.path === item.path"
+			class="mr-4 mt-2"
+			@click="handleClick(item)"
+		>
+			{{ item.label }}
+		</b-nav-item>
 	</b-nav>
 </template>
 
@@ -13,46 +20,67 @@ export default {
 	components: {
 		XIcon,
 	},
+
 	props: {
 		items: {
-			type: 'Array',
+			type: Array,
 			default: () => [],
 			desciption: 'Lorem ipsum',
 			required: true,
+		},
+	},
+
+	data() {
+		return {
+			activeItem: this.items[0],
+		};
+	},
+
+	computed: {
+		computedItems() {
+			let count = 0;
+			return this.items.map((item, index) => {
+				if(item.active) {
+					count = count + 1;
+				}
+				if(count > 0 && index > 0) {
+					delete item.active;
+				}
+				return item;
+			});
 		}
 	},
+
+	methods: {
+		handleClick(item) {
+			this.activeItem = item;
+			this.$emit('click', this.activeItem);
+		}
+	}
 };
 </script>
 <style>
-.toolbar-container {
-	width: 100%;
-	display: flex;
-	justify-content: center;
-}
+	.nav {
+		background: linear-gradient(0.93deg, #FFFFFF 16.45%, #F1F5FB 105.67%);
+	}
 
-.toolbar {
-	border-radius: 8px !important;
-	background-color: #30353F;
-	bottom: 72px;
-	padding: 0.8rem 1rem;
-	color: #fff;
-	display: flex;
-	z-index: 2;
-	align-items: center;
-}
+	.nav-link {
+		color: #6A7580;
+		padding: 8px;
+	}
 
-.fixed {
-	position: fixed;
-}
+	.nav-link:hover {
+		color: #142032;
+	}
 
-.icon-container {
-	padding: 2px 4px;
-	background-color: #0000002d;
-	border-radius: 50%;
-	cursor: pointer;
-}
+	.active {
+		border-bottom: 4px solid rgb(40, 90, 185);
+		background: rgb(40, 90, 185, 0.05);
+		border-radius: 4px  4px 0px 0px;
+		color: #142032;
+	}
 
-.icon-container:hover {
-	background-color: #00000041;
-}
+	.active:hover {
+		color:#142032;
+	}
 </style>
