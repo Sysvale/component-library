ad390759c7b1afdcb0a858bc88bf101ac9a18e18
Tássio Caique <tassiok2@gmail.com>
2020-06-22 14:52:24 -0300

    Add - Estilização condicional

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,13 @@
 <template>
 	 <b-nav
 	 	class="px-2"
+		:style="styleVariables"
 	 >
 		<b-nav-item
 			v-for="(item, index) in items"
 			:id="getElementKey(item, index)"
 			:key="getElementKey(item, index)"
-			:active="activeItem.path === item.path"
+			:active="isActive(item)"
 			class="mr-4 mt-2"
 			@click="handleClick(item)"
 		>
@@ -24,6 +25,11 @@ export default {
 			desciption: 'Lorem ipsum',
 			required: true,
 		},
+		oldSchool: {
+			type: Boolean,
+			default: false,
+			description: 'Lorem ipsum',
+		},
 	},
 
 	data() {
@@ -32,11 +38,38 @@ export default {
 		};
 	},
 
+	computed: {
+		styleVariables() {
+			if(this.oldSchool) {
+				return {
+					'--active-color': 'rgb(58, 223, 124)',
+					'--active-color-opacity': 'rgb(90, 90, 90)',
+					'--active-text-color': 'rgb(255, 255, 255)',
+					'--text-color': 'rgb(208, 210, 213)',
+					'--background-color': 'rgb(68, 68, 68)',
+				};
+			}
+
+			return {
+				'--active-color': 'rgb(40, 90, 185)',
+				'--active-color-opacity': 'rgba(40, 90, 185, 0.05)',
+				'--active-text-color': 'rgb(20, 32, 50)',
+				'--text-color': 'rgb(106, 117, 128)',
+				'--background-color': 'linear-gradient(0.93deg, #FFFFFF 16.45%, #F1F5FB 105.67%)',
+			};
+		}
+	},
+
 	methods: {
 		handleClick(item) {
 			this.activeItem = item;
 			this.$emit('click', this.activeItem);
 		},
+
+		isActive(item) {
+			return this.activeItem.path === item.path;
+		},
+
 		getElementKey(item, index) {
 			return `${item.label.replace(/\s/g, '')}${index}`;
 		}
@@ -45,26 +78,26 @@ export default {
 </script>
 <style>
 	.nav {
-		background: linear-gradient(0.93deg, #FFFFFF 16.45%, #F1F5FB 105.67%);
+		background: var(--background-color);
 	}
 
 	.nav-link {
-		color: #6A7580;
+		color: var(--text-color);
 		padding: 8px;
 	}
 
 	.nav-link:hover {
-		color: #142032;
+		color: var(--active-text-color);
 	}
 
 	.active {
-		border-bottom: 4px solid rgb(40, 90, 185);
-		background: rgb(40, 90, 185, 0.05);
+		border-bottom: 4px solid var(--active-color);
+		background: var(--active-color-opacity);
 		border-radius: 4px  4px 0px 0px;
-		color: #142032;
+		color: var(--active-text-color);
 	}
 
 	.active:hover {
-		color:#142032;
+		color:var(--active-text-color);
 	}
 </style>
