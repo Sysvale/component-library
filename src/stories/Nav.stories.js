8cc8e112f9a953911eac8ed75366e1597d0f1720
Tássio Caique <tassiok2@gmail.com>
2020-06-23 09:48:00 -0300

    Add - troca stepper por nav

--- src/stories/Nav.stories.js
+++ src/stories/Nav.stories.js
@@ -16,7 +16,7 @@ const template = `
 	@click="handleClick"
 />`;
 
-const componentDescription = 'Stepper counters are components that control and display the steps of a stepper.';
+const componentDescription = 'Nav counters are components that control and display the steps of a Nav.';
 
 const docsDecorator = () => {
 	return {
@@ -31,20 +31,20 @@ const docsDecorator = () => {
 			`<div>
 				<wrapper :componentData="component" :exampleSourceCode="template" :componentDescription="componentDescription" componentName="Nav">
 					<div slot="usage">
-						<h5>Use Stepper Controllers when:</h5>
+						<h5>Use Nav Controllers when:</h5>
 						<ul>
-							<li>You want to show, in a visual way, the current state and the current step of a stepper</li>
+							<li>You want to show, in a visual way, the current state and the current step of a Nav</li>
 							<li>You have a multistep modal</li>
 							<li>You have a series of inputs or actions that you want to show the progress of the filling and the hour and date of the states or the actions aren't necessary</li>
 						</ul>
 						<br>
-						<h5>Don't use Stepper Controllers when:</h5>
+						<h5>Don't use Nav Controllers when:</h5>
 						<ul>
 							<li>You are looking for a timeline component</li>
 							<li>The order of the steps, events, or actions are relevant</li>
 						</ul>
 
-						<p class="mt-5	">Obs.: When clicked, the stepper emits an event called 'step_changed' that sends to to you the index of the step clicked,
+						<p class="mt-5	">Obs.: When clicked, the Nav emits an event called 'step_changed' that sends to to you the index of the step clicked,
 						and the data of this step.</p>
 					</div>
 					<story slot="component-preview"/>
