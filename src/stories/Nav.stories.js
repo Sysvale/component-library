3bae8cb104cddbeef3f454c4d13f9bee37f267d3
Rafael Dias <rdias.ga@gmail.com>
2020-06-29 14:02:31 -0300

    A dequa o stories do Nav ao novo padrão do DocsWrapper

--- src/stories/Nav.stories.js
+++ src/stories/Nav.stories.js
@@ -8,15 +8,38 @@ import {
 import Nav from '../components/Nav.vue';
 import { action } from '@storybook/addon-actions';
 
-const template = `
-<s-nav
-	:items="items"
-	:old-school="oldSchool"
-	:active-item="activeItem"
-	@click="handleClick"
-/>`;
+console.log('Nav: ', Nav);
 
-const componentDescription = 'Nav are components used to create navigation solutions and menus.';
+// ------ COMPONENT INFO ------
+const template = `
+    <s-nav
+        :items="items"
+        :old-school="oldSchool"
+        :active-item="activeItem"
+        @click="handleClick"
+    />`;
+
+const componentDescription = {
+    name: 'Nav',
+    summary: 'Nav are components used to create navigation solutions and menus.',
+    usage: {
+        whenToUSe: [
+            'You need to separate content by pages.', 
+            `It’s not appropriate to divide content using tabs, sidebars or using 
+            cards and links in one page to navigate to another.`
+        ],
+        whenNotUse:[
+            'You have too many items to be displayed in the menu.',
+            'You want to show the links vertically.'
+        ]
+	},
+	events: {
+		click: {
+			trigger: 'click',
+			description: 'Event emitted when one of the menu items is clicked.'
+		}
+	},
+};
 
 const docsDecorator = () => {
 	return {
@@ -28,37 +51,21 @@ const docsDecorator = () => {
 			};
 		},
 		template:
-			`<div>
-				<docs-wrapper :componentData="component" :exampleSourceCode="template" :componentDescription="componentDescription" componentName="Nav">
-					<div slot="usage">
-						<h5>Use Navs when:</h5>
-						<ul>
-							<li>
-								You need to separate content by pages.
-							</li>
-							<li>
-								It’s not appropriate to divide content using tabs, sidebars or using cards and links in one page to navigate to another.
-							</li>
-						</ul>
-						<br>
-						<h5>Don't use Navs when:</h5>
-						<ul>
-							<li>You have too many items to be displayed in the menu.</li>
-							<li>You want to show the links vertically.</li>
-						</ul>
-
-						<p class="mt-5">Obs.: When you click in an item an event called 'click' will be emitted and the item itself is passed as argument</p>
-					</div>
-					<story slot="component-preview"/>
-				</docs-wrapper>
-			</div>`,
+			`<docs-wrapper
+				:componentData="component"
+				:exampleSourceCode="template"
+				:componentDescription="componentDescription"
+			>
+				<story slot="component-preview"/>
+			</docs-wrapper>`,
 	};
 };
 
+// ------ STORYBOOK SETTINGS ------
 export default {
 	component: Nav,
 	title: 'cs/Nav',
-	decorators: [docsDecorator, withKnobs, withA11y],
+	decorators: [docsDecorator, withKnobs, withA11y, withDesign],
 	parameters: {
 		a11y: {
 			element: '.preview-container',
@@ -70,6 +77,7 @@ export default {
 	},
 };
 
+// ------ KNOBS SETTINGS ------
 const items = [
 	{
 		label: 'Mapa de grupo de risco',
