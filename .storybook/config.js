acb7b58b6556032124eb5d93fbae56166862132f
Rafael Dias <rdias.ga@gmail.com>
2020-07-08 10:43:47 -0300

    Importa e registra o multiselect

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure, addParameters } from '@storybook/vue';
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import Multiselect from 'vue-multiselect'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import { CheckCircleIcon } from 'vue-feather-icons';
@@ -28,6 +29,7 @@ Vue.component('s-action-bar', ActionBar);
 Vue.component('check-circle-icon', CheckCircleIcon);
 Vue.component('s-empty-state', EmptyState);
 Vue.component('s-nav', Nav);
+Vue.component('multiselect', Multiselect)
 
 Vue.use(BootstrapVue);
 
