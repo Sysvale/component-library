7762ea602170e974e8a4774aa09c3beec8931234
Rafael Dias <rdias.ga@gmail.com>
2020-07-08 13:38:44 -0300

    Implementa o dropdown com checkbox

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,36 +1,39 @@
 <template>
+<div>
 	<multiselect
 		v-model="value"
 		v-bind="$attrs"
-		placeholder="Fav No Man’s Sky path"
+		placeholder="Selecione quantas opções quiser"
 		label="title"
 		track-by="title"
 		:options="options"
 		:option-height="104"
-		:custom-label="customLabel"
 		:close-on-select="false"
-		:show-labels="false"
+		:multiple="true"
+		tag-placeholder="Add this as new tag"
+		:taggable="true"
+		@select="selectItem"
+		@remove="selectItem"
+		selectLabel=''
+		:clearOnSelect="false"
+		:clear-on-select="false"
+		:closeOnSelect="false"
+		deselectLabel=''
+		selectedLabel=''
+		:searchable="false"
 	>
-		<template slot="singleLabel" slot-scope="props">
-			<x-icon
-				size="0.8x"
-				class="m-1"
-			/>
-			Nome
-		</template>
 		<template
 			slot="option"
 			slot-scope="props"
 		>
-        <div
+		<div
 		  	class="option__desc"
-			@click="status = !status"
 		>
 			<span class="option__title">
-				<span class="d-flex">
+				<span class="d-flex align-self-center align-items-center align-content-center">
 					<b-form-checkbox
 						id="checkbox-1"
-						v-model="status"
+						v-model="props.option.is_selected"
 						name="checkbox-1"
 						:value="true"
 						:unchecked-value="false"
@@ -39,8 +42,9 @@
 				</span>
 			</span>
 		</div>
-        </template>
+		</template>
   </multiselect>
+</div>
 </template>
 
 <script>
@@ -57,16 +61,74 @@ export default {
 		return {
 			internalActiveItem: this.activeItem,
 			internalActiveParent: this.activeItem,
-			value: { title: 'Explorer', desc: 'Discovering new species!', img: 'static/posters/creatures.png' },
+			value: [],
 			options: [
-				{ title: 'Space Pirate', desc: 'More space battles!', img: 'static/posters/fleet.png' },
-				{ title: 'Merchant', desc: 'PROFIT!', img: 'static/posters/trading_post.png' },
-				{ title: 'Explorer', desc: 'Discovering new species!', img: 'static/posters/creatures.png' },
-				{ title: 'Miner', desc: 'We need to go deeper!', img: 'static/posters/resource_lab.png' }
+				{ title: 'Space Pirate', is_selected: false, img: 'static/posters/fleet.png' },
+				{ title: 'Merchant', is_selected: false, img: 'static/posters/trading_post.png' },
+				{ title: 'Explorer', is_selected: false, img: 'static/posters/creatures.png' },
+				{ title: 'Miner', is_selected: false, img: 'static/posters/resource_lab.png' }
 			],
 			status: false,
 		}
 	},
+
+	methods: {
+		selectItem (newTag) {
+			console.log('newTag: ', newTag);
+			newTag.is_selected = !newTag.is_selected;
+		}
+  }
 };
 </script>
 <style src="vue-multiselect/dist/vue-multiselect.min.css" />
+<style>
+.multiselect__option--highlight {
+	background: #F5F6F8;
+	outline: none;
+	color: #142032;
+}
+
+.custom-control-input:checked ~ .custom-control-label::before {
+	color: #fff;
+	border-color: #285AB5;
+	background-color: #285AB5;
+}
+
+.multiselect__tag {
+	background: #e9eef1;
+	color: #142032;
+	border: 1px solid #ced4da;
+}
+
+	
+.multiselect__tag-icon:after{
+	color: #4F606F;
+}
+
+.multiselect__tag-icon:focus,
+.multiselect__tag-icon:hover {
+	background: #d1d9e9;
+	color: #4F606F;
+}
+
+.multiselect__tag-icon:focus:after,
+.multiselect__tag-icon:hover:after {
+	color: #4F606F;
+}
+
+.multiselect__option--selected.multiselect__option--highlight {
+	background: #fff;
+	color: #142032;
+}
+.multiselect__option--selected.multiselect__option--highlight:after {
+	background: #fff;
+	content: attr(data-deselect);
+	color: #142032;
+}
+
+.multiselect__option--selected {
+	background: #FFF;
+	color: #142032;
+	font-weight: 600;
+}
+</style>
