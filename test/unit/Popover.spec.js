c576729da872ab48f1e9f3eac1db4f5724090cda
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-09-04 10:05:21 -0300

    Add testes para verificar alinhamento do popover

--- test/unit/Popover.spec.js
+++ test/unit/Popover.spec.js
@@ -1,29 +1,73 @@
 import { mount, createLocalVue } from '@vue/test-utils';
-import BootstrapVue, { BButton } from 'bootstrap-vue';
+import BootstrapVue from 'bootstrap-vue';
 import Popover from '../../src/components/Popover.vue';
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
-const MyButton = {
-	template: `
-		<b-button id="trigger-popover" @click="">
-			Click Me!
-		</b-button>
-	`,
-};
-const button = mount(MyButton);
-
-
-test('Component is mounted properly', async () => {
+test('Component is mounted properly', () => {
 	const wrapper = mount(Popover, {
 		localVue,
 		propsData: {
-			target: 'target-popover',
-			right: true,
+			target: 'trigger-popover',
 		},
 	});
 
-	await button.trigger('click');
 	expect(wrapper).toMatchSnapshot();
+});
+
+describe("Items styles test", () => {
+	let App;
+	beforeEach(() => {
+		App = localVue.component('App', {
+			components: { Popover },
+			props: {
+				right: {
+					type: Boolean,
+					default: false,
+				},
+				left: {
+					type: Boolean,
+					default: true,
+				},
+			},
+			template: `
+				<div>
+					<button id="trigger-popover"> Click </button>
+					<popover
+						target="trigger-popover"
+						size="lg"
+						:right="right"
+						:left="left"
+					/>
+				</div>
+			`,
+		});
+	});
+
+	it('if component is left aligned internalOffset must be negative', () => {
+		const wrapper = mount(App, {
+			localVue,
+			propsData: {
+				left: true,
+			},
+			attachTo: document.body,
+		});
+		const offset = wrapper.findComponent(Popover).vm.internalOffset;
+		const offsetNegative = offset < 0;
+		expect(offsetNegative).toBe(true);
+	});
+	
+	it('if component is right aligned internalOffset must be positive', () => {
+		const wrapper = mount(App, {
+			localVue,
+			propsData: {
+				right: true,
+			},
+			attachTo: document.body,
+		});
+		const offset = wrapper.findComponent(Popover).vm.internalOffset;
+		const offsetNegative = offset < 0;
+		expect(offsetNegative).toBe(false);
+	});
 });
