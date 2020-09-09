7b9617c978536f7c84ccf72065b3cc613012a044
Rafael Dias <rdias.ga@gmail.com>
2020-09-09 11:07:10 -0300

    Modifica a descrição do popover

--- src/stories/Popover.stories.js
+++ src/stories/Popover.stories.js
@@ -23,8 +23,11 @@ const template = `
 				:alignment="alignment"
 			>
 				<p class="p-3">
-					Mussum Ipsum, cacilds vidis litro abertis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Atirei o pau no gatis, per gatis num morreus. Pra lá , depois divoltis porris, paradis. Leite de capivaris, leite de mula manquis sem cabeça.
-					Viva Forevis aptent taciti sociosqu ad litora torquent.
+					Mussum Ipsum, cacilds vidis litro abertis. Todo mundo vê os porris que eu tomo,
+					mas ninguém vê os tombis que eu levo! Atirei o pau no gatis, per gatis num
+					morreus. Pra lá , depois divoltis porris, paradis. Leite de capivaris,
+					leite de mula manquis sem cabeça. Viva Forevis aptent taciti sociosqu
+					ad litora torquent.
 				</p>
 			</s-popover>
 		</div>
@@ -32,20 +35,23 @@ const template = `
 
 const componentDescription = {
 		name: 'Popover',
-		summary: 'Popover is a component emulate a tooltip behavior, can be applied to any interactive element. This component supports the click interaction.',
+		summary: `Popover is a pop-up box that appears when the user clicks on an element.
+			It is used to emulate a tooltip behavior, and to display content to the user
+			that was previously hidden.`,
 		usage: {
 				whenToUse: [
-						'You want to place filters or sort fields that will be shown when you click in some component.', 
+					'You want to place filters, or text that will be shown when you click in some component.', 
 				],
 				whenNotUse:[
-						'You need a modal.',
-						'You need a navigation solution.',
-						'You are need a form to register something.'
+					'You need a modal.',
+					'You need a navigation solution.',
+					'You need a form to register something.',
+					'You need to show a lot of content.'
 				]
 	},
 	slots: {
 		default: {		
-			description: 'Slot used to display the content inside the component.'
+			description: 'Slot used to display the content inside the popover.'
 		},
 	},
 };
