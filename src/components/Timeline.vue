9de33654da51e748945682832de5dfd63c0cac33
Rafael Dias <rdias.ga@gmail.com>
2020-09-22 15:30:44 -0300

    Cria componente de timeline

---
+++ src/components/Timeline.vue
@@ -0,0 +1,198 @@
+<template>
+	<div
+		class="panel-body-content"
+	>
+		<div
+			v-for="(event, index) in history"
+			:key="index"
+		>
+			<div class="event-row">
+				<div>
+					<p
+						v-if="showDate"
+						class="regular fs-14 text-secondary date-text"
+					>
+						{{ event.date }}
+					</p>
+				</div>
+				<div
+					class="timeline-container"
+				>
+					<span
+						class="event-pin"
+					/>
+					<div
+						v-if="((index + 1) < history.length) && (history.length > 1)"
+						class="time-line"
+					/>
+				</div>
+				<div class="event-info-container">
+					<div>
+						<p
+							class="mb-0 event-title regular">
+							{{ event.title }}
+						</p>
+					</div>
+					<div class="light fs-14 mt-1">
+						{{ event.text }}
+					</div>
+					<hr
+						v-if="(index + 1) < history.length"
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { XIcon } from 'vue-feather-icons'
+export default {
+	components: {
+		XIcon,
+	},
+	
+	props: {
+		history: {
+			type: Array,
+			default: [
+				{
+					date: '22/09/2020',
+					title: 'Bloqueio nível 1',
+					text: 'Usuário: Uncle Bob',
+				},
+				{
+					date: '22/09/2020',
+					title: 'Bloqueio nível 1',
+					text: 'Usuário: Uncle Bob',
+				},
+				{
+					date: '22/09/2020',
+					title: 'Bloqueio nível 1',
+					text: 'Usuário: Uncle Bob',
+				},
+				{
+					date: '22/09/2020',
+					title: 'Bloqueio nível 1',
+					text: 'Usuário: Uncle Bob',
+				},
+				{
+					date: '22/09/2020',
+					title: 'Bloqueio nível 1',
+					text: 'Usuário: Uncle Bob',
+				},
+			],
+			description: 'The text that will be displayed inside the badge.',
+			required: true,
+		},
+		showDate: {
+			type: Boolean,
+			default: true,
+		}
+	},
+
+	computed: {
+		predefinedStyle() {
+			let computed_style = '';
+			switch (this.variant) {
+				case 'yellow':
+					computed_style = 'yellow';
+					break;
+				case 'green':
+					computed_style = 'green';
+					break;
+				case 'red':
+					computed_style = 'red';
+					break;
+				case 'blue':
+					computed_style = 'blue';
+					break;
+				case 'purple':
+					computed_style = 'purple';
+					break;
+				case 'gray':
+					computed_style = 'gray';
+					break;
+				default:
+					computed_style = 'yellow';
+					break;
+			}
+
+			return computed_style;
+		},
+
+		styleVariables() {
+			return {
+				'--bg-color': this.bgColor,
+				'--color': this.color,
+			};
+		},
+	},
+};
+</script>
+<style>
+
+.panel-body-content {
+	padding: 24px;
+	overflow: auto;
+    max-height: 350px;
+	width: 100%;
+	scrollbar-color: #CDCDCD #FFFFFF;
+	scrollbar-width: thin;
+}
+
+.event-title {
+	color: #182532;
+	font-weight: 600;
+}
+
+.event-pin {
+	height: 12px;
+	width: 11.5px;
+	border-radius: 50%;
+	background-color: #2EB88D;
+    border: 1px solid #2EB88D;
+}
+
+.time-line {
+	height: 100%;
+    width: 1px;
+    background-color: #CDCDCD;
+}
+
+.timeline-container {
+	display: flex;
+    flex-direction: column;
+	align-items: center;
+}
+
+.event-row {
+	display: flex;
+    justify-content: space-between;
+}
+
+.event-info-container {
+	width: 70%;
+	margin-top: -8px;
+}
+
+.panel-body-content::-webkit-scrollbar {
+	width: 0.35rem;
+}
+
+.panel-body-content::-webkit-scrollbar-thumb {
+	border-radius: 4px;
+	background: #CDCDCD#FFFFFF
+}
+
+.date-text {
+	text-transform: Capitalize;
+	align-self: flex-end;
+	margin-top: -4px;
+	margin-bottom: 0px;
+}
+
+.light {
+	color: #697580;
+}
+</style>
